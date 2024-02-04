6495f68b29d8356ba10053dfeb2002e8b17bf88a
thaina monteles silva <thmonteles@gmail.com>
2024-02-04 16:06:04 -0300

    implement company class

---
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/models/Company.java
@@ -0,0 +1,48 @@
+package br.com.tgid.TgidTestJavaDeveloper.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import org.hibernate.validator.constraints.br.CNPJ;
+
+
+@Entity
+public class Company extends User {
+
+    @NotBlank(message = "This field cnpj cannot be empty")
+    @Column(unique = true)
+    @CNPJ(message = "Invalid CNPJ format")
+    private String cnpj;
+    private double balance;
+    private double fee;
+
+    public Company() {
+        setUserType(UserType.COMPANY);
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+}
+
