cb5dd8bf2976fb7e3c218ea0d9c444061a22f501
thaina monteles silva <thmonteles@gmail.com>
2024-02-04 13:19:39 -0300

    Add base spring boot project

---
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/TgidTestJavaDeveloperApplication.java
@@ -0,0 +1,13 @@
+package br.com.tgid.TgidTestJavaDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgidTestJavaDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgidTestJavaDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tgid/TgidTestJavaDeveloper/TgidTestJavaDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tgid.TgidTestJavaDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgidTestJavaDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
