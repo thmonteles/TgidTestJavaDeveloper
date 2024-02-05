a6e776d9350580a9be42f81d90bba01125758bdf
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 00:10:53 -0300

    add company repository

---
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/repositories/CompanyRepository.java
@@ -0,0 +1,13 @@
+package br.com.tgid.TgidTestJavaDeveloper.repositories;
+
+import br.com.tgid.TgidTestJavaDeveloper.models.Client;
+import br.com.tgid.TgidTestJavaDeveloper.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByCnpj(String cnpj);
+}
