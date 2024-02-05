2811dc7aa5f3477a8c3980ce40856e50beee48e9
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 03:33:32 -0300

    implement transaction service

---
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/TransactionServiceImpl.java
@@ -0,0 +1,73 @@
+package br.com.tgid.TgidTestJavaDeveloper.services.clientImpl;
+
+
+import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateTransactionDTO;
+import br.com.tgid.TgidTestJavaDeveloper.exceptions.TransactionServiceException;
+import br.com.tgid.TgidTestJavaDeveloper.models.Company;
+import br.com.tgid.TgidTestJavaDeveloper.models.Transaction;
+import br.com.tgid.TgidTestJavaDeveloper.repositories.ClientRepository;
+import br.com.tgid.TgidTestJavaDeveloper.repositories.CompanyRepository;
+import br.com.tgid.TgidTestJavaDeveloper.repositories.TransactionRepository;
+import br.com.tgid.TgidTestJavaDeveloper.services.TransactionService;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class TransactionServiceImpl implements TransactionService {
+    private final TransactionRepository transactionRepository;
+    private final CompanyRepository companyRepository;
+    private final ClientRepository clientRepository;
+
+    public TransactionServiceImpl(TransactionRepository transactionRepository, CompanyRepository companyRepository, ClientRepository clientRepository) {
+        this.transactionRepository = transactionRepository;
+        this.companyRepository = companyRepository;
+        this.clientRepository = clientRepository;
+    }
+
+    public Transaction process(CreateTransactionDTO dto) throws TransactionServiceException {
+        var client = clientRepository.findByCpf(dto.clientCpf())
+                .orElseThrow(() -> new TransactionServiceException("client not found for this cpf: " + dto.clientCpf()));
+
+        var company = companyRepository
+                .findByCnpj(dto.companyCnpj())
+                .orElseThrow(() -> new TransactionServiceException("company not found for this cnpj: " + dto.companyCnpj()));
+
+        var calculatedFeeFromAmount = calculateDynamicFeeFromCompanyFee(company, dto.amount());
+        var totalAmountForTransaction = calculateFinalAmountFromFee(dto.amount(), calculatedFeeFromAmount);
+        var checkIfBalanceIsSufficientToTransaction = balanceIsSufficient(company, totalAmountForTransaction);
+
+        if (checkIfBalanceIsSufficientToTransaction)
+            throw new TransactionServiceException("balance amount from this company is insufficient for this transaction");
+
+        var transaction = Transaction.from(company, client, totalAmountForTransaction);
+        updateCompanyBalance(company, totalAmountForTransaction);
+        return transactionRepository.save(transaction);
+    }
+
+
+    private static BigDecimal calculateDynamicFeeFromCompanyFee(Company company, BigDecimal amount) {
+        return amount
+                .multiply(BigDecimal.valueOf(company.getFee()));
+    }
+
+    private static BigDecimal calculateFinalAmountFromFee(BigDecimal rawAmount, BigDecimal fee) {
+        return rawAmount.add(fee);
+    }
+
+    private static boolean balanceIsSufficient(Company company, BigDecimal amount) {
+        return company
+                .getBalance()
+                .compareTo(amount) < 0;
+    }
+
+    private void updateCompanyBalance(Company company, BigDecimal totalAmountForTransaction) {
+        var newBalance = company
+                .getBalance()
+                .add(totalAmountForTransaction);
+
+        company.setBalance(newBalance);
+        companyRepository.save(company);
+    }
+
+}
