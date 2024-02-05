c552cbfd225a61e4e40cda94440bdeba234223bf
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 02:04:43 -0300

    add list all companies on company service

--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/CompanyService.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/CompanyService.java
@@ -1,8 +1,13 @@
 package br.com.tgid.TgidTestJavaDeveloper.services;
 
+import br.com.tgid.TgidTestJavaDeveloper.DTOs.CompanyResponseDTO;
 import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateCompanyDTO;
 import br.com.tgid.TgidTestJavaDeveloper.exceptions.CompanyServiceException;
 
+import java.util.List;
+
 public interface CompanyService {
     boolean create(CreateCompanyDTO dto) throws CompanyServiceException;
+    public List<CompanyResponseDTO> listAllCompanies() throws CompanyServiceException;
+
 }
