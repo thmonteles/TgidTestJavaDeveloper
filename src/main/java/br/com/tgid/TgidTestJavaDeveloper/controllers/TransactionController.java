1e8fa9a1dd54105a792ecc1309c9ac30667b53fc
Thainá Monteles <thmonteles@gmail.com>
2024-02-05 06:43:05 -0300

    add transaction controller

---
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/controllers/TransactionController.java
@@ -0,0 +1,38 @@
+package br.com.tgid.TgidTestJavaDeveloper.controllers;
+
+
+import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateTransactionDTO;
+import br.com.tgid.TgidTestJavaDeveloper.DTOs.SuccessOrFailureResponse;
+import br.com.tgid.TgidTestJavaDeveloper.DTOs.TransactionResponseDTO;
+import br.com.tgid.TgidTestJavaDeveloper.exceptions.TransactionServiceException;
+import br.com.tgid.TgidTestJavaDeveloper.services.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/transaction")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+
+    @PostMapping("/create")
+    public ResponseEntity<TransactionResponseDTO> processTransaction(@RequestBody CreateTransactionDTO createTransactionDTO) {
+        try {
+            var transactionResult = transactionService.process(createTransactionDTO);
+            if (transactionResult != null) {
+                return ResponseEntity.ok(new TransactionResponseDTO(true, "Transaction processed successfully", Optional.of(transactionResult)));
+            }
+            return ResponseEntity.badRequest().body(new TransactionResponseDTO(false, "Transaction processing failed", Optional.empty()));
+        } catch (TransactionServiceException e) {
+            return ResponseEntity.badRequest().body(new TransactionResponseDTO(false, e.getMessage(), Optional.empty()));
+        }
+    }
+}
