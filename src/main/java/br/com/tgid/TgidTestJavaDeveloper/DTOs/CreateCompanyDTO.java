c0807dd6283814c843f0c7b7ec747d2dad8b6547
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 02:03:01 -0300

    fix create company DTO

--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/DTOs/CreateCompanyDTO.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/DTOs/CreateCompanyDTO.java
@@ -3,5 +3,5 @@ package br.com.tgid.TgidTestJavaDeveloper.DTOs;
 import jakarta.validation.constraints.NotBlank;
 
 
-public record CreateCompanyDTO(@NotBlank String cnpj, @NotBlank double fee, @NotBlank String email, @NotBlank String phone, @NotBlank String password) {
+public record CreateCompanyDTO(@NotBlank String cnpj, @NotBlank double fee, @NotBlank String name, @NotBlank String email, @NotBlank String phone, @NotBlank String password) {
 }
