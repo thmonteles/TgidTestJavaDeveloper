aa5fde36162b30f6d087175c1635a1df83e06cf2
Thainá Monteles <thmonteles@gmail.com>
2024-02-05 08:41:21 -0300

    enable thread pool executor with async mode

--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/TgidTestJavaDeveloperApplication.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/TgidTestJavaDeveloperApplication.java
@@ -2,7 +2,13 @@ package br.com.tgid.TgidTestJavaDeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.concurrent.Executor;
+
+@EnableAsync
 @SpringBootApplication
 public class TgidTestJavaDeveloperApplication {
 
@@ -10,4 +16,14 @@ public class TgidTestJavaDeveloperApplication {
 		SpringApplication.run(TgidTestJavaDeveloperApplication.class, args);
 	}
 
+	@Bean
+	public Executor asyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(2);
+		executor.setQueueCapacity(500);
+		executor.setThreadNamePrefix("TgidAsync-");
+		executor.initialize();
+		return executor;
+	}
 }
