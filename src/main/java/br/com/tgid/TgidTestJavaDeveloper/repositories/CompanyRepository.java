f2423777159f8926283fea8e7195eb391e34b098
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 00:20:55 -0300

    add company service interface

--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/repositories/CompanyRepository.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/repositories/CompanyRepository.java
@@ -1,6 +1,5 @@
 package br.com.tgid.TgidTestJavaDeveloper.repositories;
 
-import br.com.tgid.TgidTestJavaDeveloper.models.Client;
 import br.com.tgid.TgidTestJavaDeveloper.models.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/CompanyService.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/CompanyService.java
@@ -1,7 +1,8 @@
 package br.com.tgid.TgidTestJavaDeveloper.services;
 
 import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateCompanyDTO;
+import br.com.tgid.TgidTestJavaDeveloper.exceptions.CompanyServiceException;
 
 public interface CompanyService {
-    boolean create(CreateCompanyDTO dto);
+    boolean create(CreateCompanyDTO dto) throws CompanyServiceException;
 }
