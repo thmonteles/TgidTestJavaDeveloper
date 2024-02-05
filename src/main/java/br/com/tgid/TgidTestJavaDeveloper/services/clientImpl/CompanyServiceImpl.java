927a7f6e405df7dd5c249d5d93e53615bd03f332
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 00:28:59 -0300

    implement findByCnpj in company service and save new company

--- src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/CompanyServiceImpl.java
+++ src/main/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/CompanyServiceImpl.java
@@ -1,13 +1,25 @@
 package br.com.tgid.TgidTestJavaDeveloper.services.clientImpl;
 
 import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateCompanyDTO;
+import br.com.tgid.TgidTestJavaDeveloper.exceptions.CompanyServiceException;
+import br.com.tgid.TgidTestJavaDeveloper.models.Company;
+import br.com.tgid.TgidTestJavaDeveloper.repositories.CompanyRepository;
 import br.com.tgid.TgidTestJavaDeveloper.services.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CompanyServiceImpl implements CompanyService {
+    @Autowired
+    CompanyRepository companyRepository;
+
     @Override
-    public boolean create(CreateCompanyDTO dto) {
-        return false;
+    public boolean create(CreateCompanyDTO dto) throws CompanyServiceException {
+        var company = companyRepository.findByCnpj(dto.cnpj());
+        if (company.isPresent()) {
+            throw new CompanyServiceException("company already exists with this cnpj: " + dto.cnpj());
+        }
+        var newCompany = Company.fromDTO(dto);
+        return companyRepository.save(newCompany) != null;
     }
 }
