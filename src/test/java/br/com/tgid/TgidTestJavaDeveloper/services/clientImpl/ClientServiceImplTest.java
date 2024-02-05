f3765e9ef2368976273fbf04d1ac2fc216a3b4f7
thaina monteles silva <thmonteles@gmail.com>
2024-02-05 02:28:33 -0300

    fix tests with new changes on models and repository

--- src/test/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/ClientServiceImplTest.java
+++ src/test/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/ClientServiceImplTest.java
@@ -4,6 +4,7 @@ import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateClientDTO;
 import br.com.tgid.TgidTestJavaDeveloper.exceptions.ClientServiceException;
 import br.com.tgid.TgidTestJavaDeveloper.models.Client;
 import br.com.tgid.TgidTestJavaDeveloper.repositories.ClientRepository;
+import br.com.tgid.TgidTestJavaDeveloper.utils.CPFUtil;
 import org.junit.jupiter.api.Test;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,11 +17,11 @@ class ClientServiceImplTest {
         ClientRepository clientRepository = mock(ClientRepository.class);
         ClientServiceImpl clientService = new ClientServiceImpl(clientRepository);
         CreateClientDTO validDto = new CreateClientDTO("John Doe", "123.456.789-09", "john.doe@example.com", "some-password", "123456789");
-        when(clientRepository.findByEmail(validDto.email())).thenReturn(Optional.empty());
+        when(clientRepository.findByCpf(CPFUtil.cleaning(validDto.cpf()))).thenReturn(Optional.empty());
         when(clientRepository.save(any(Client.class))).thenReturn(new Client());
         boolean result = clientService.create(validDto);
         assertTrue(result);
-        verify(clientRepository, times(1)).findByEmail(validDto.email());
+        verify(clientRepository, times(1)).findByCpf(CPFUtil.cleaning(validDto.cpf()));
         verify(clientRepository, times(1)).save(any(Client.class));
     }
 
@@ -30,18 +31,19 @@ class ClientServiceImplTest {
         ClientServiceImpl clientService = new ClientServiceImpl(clientRepository);
         CreateClientDTO invalidDto = new CreateClientDTO("John Doe", "invalid-cpf", "john.doe@example.com", "some-password", "123456789");
         assertThrows(ClientServiceException.class, () -> clientService.create(invalidDto));
-        verify(clientRepository, never()).findByEmail(anyString());
+        verify(clientRepository, never()).findByCpf(anyString());
         verify(clientRepository, never()).save(any(Client.class));
     }
 
     @Test
-    void testCreateClientWithExistingEmail() {
+    void testCreateClientWithExistingCpf() {
         ClientRepository clientRepository = mock(ClientRepository.class);
         ClientServiceImpl clientService = new ClientServiceImpl(clientRepository);
-        CreateClientDTO existingEmailDto = new CreateClientDTO("John Doe", "123.456.789-09", "existing.email@example.com", "some-password", "123456789");
-        when(clientRepository.findByEmail(existingEmailDto.email())).thenReturn(Optional.of(new Client()));
-        assertThrows(ClientServiceException.class, () -> clientService.create(existingEmailDto));
-        verify(clientRepository, times(1)).findByEmail(existingEmailDto.email());
+        CreateClientDTO existingCpfDto = new CreateClientDTO("John Doe", "123.456.789-09", "existing.email@example.com", "some-password", "123456789");
+        when(clientRepository.findByCpf(CPFUtil.cleaning(existingCpfDto.cpf())))
+                .thenReturn(Optional.of(new Client()));
+        assertThrows(ClientServiceException.class, () -> clientService.create(existingCpfDto));
+        verify(clientRepository, times(1)).findByCpf(CPFUtil.cleaning(existingCpfDto.cpf()));
         verify(clientRepository, never()).save(any(Client.class));
     }
 
--- src/test/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/CompanyServiceImplTest.java
+++ src/test/java/br/com/tgid/TgidTestJavaDeveloper/services/clientImpl/CompanyServiceImplTest.java
@@ -4,6 +4,7 @@ import br.com.tgid.TgidTestJavaDeveloper.DTOs.CreateCompanyDTO;
 import br.com.tgid.TgidTestJavaDeveloper.exceptions.CompanyServiceException;
 import br.com.tgid.TgidTestJavaDeveloper.models.Company;
 import br.com.tgid.TgidTestJavaDeveloper.repositories.CompanyRepository;
+import br.com.tgid.TgidTestJavaDeveloper.utils.CNPJUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,10 +16,12 @@ import static org.mockito.Mockito.*;
 class CompanyServiceImplTest {
 
     private String validCnpj;
+    private String validName;
 
     @BeforeEach
     public void setupData() {
         validCnpj = "13.505.754/0001-45";
+        validName = "testName";
     }
 
     @Test
@@ -26,14 +29,14 @@ class CompanyServiceImplTest {
         CompanyRepository companyRepository = mock(CompanyRepository.class);
         CompanyServiceImpl companyService = new CompanyServiceImpl(companyRepository);
 
-        CreateCompanyDTO validDto = new CreateCompanyDTO(validCnpj, 10.0, "company@example.com", "123456789", "some-password");
+        CreateCompanyDTO validDto = new CreateCompanyDTO(validCnpj, 10.0, validName, "company@example.com", "123456789", "some-password");
 
         when(companyRepository.findByCnpj(validDto.cnpj())).thenReturn(Optional.empty());
         when(companyRepository.save(any(Company.class))).thenReturn(new Company());
 
         boolean result = companyService.create(validDto);
         assertTrue(result);
-        verify(companyRepository, times(1)).findByCnpj(validDto.cnpj());
+        verify(companyRepository, times(1)).findByCnpj(CNPJUtil.cleaning(CNPJUtil.cleaning(validDto.cnpj())));
         verify(companyRepository, times(1)).save(any(Company.class));
     }
 
@@ -41,7 +44,7 @@ class CompanyServiceImplTest {
     void testCreateCompanyWithInvalidCNPJ() {
         CompanyRepository companyRepository = mock(CompanyRepository.class);
         CompanyServiceImpl companyService = new CompanyServiceImpl(companyRepository);
-        CreateCompanyDTO invalidDto = new CreateCompanyDTO("invalid-validCnpj", 10.0, "company@example.com", "123456789", "some-password");
+        CreateCompanyDTO invalidDto = new CreateCompanyDTO("invalid-validCnpj", 10.0, validName, "company@example.com", "123456789", "some-password");
         assertThrows(CompanyServiceException.class, () -> companyService.create(invalidDto));
         verify(companyRepository, never()).findByCnpj(anyString());
         verify(companyRepository, never()).save(any(Company.class));
@@ -51,10 +54,10 @@ class CompanyServiceImplTest {
     void testCreateCompanyWithExistingCNPJ() {
         CompanyRepository companyRepository = mock(CompanyRepository.class);
         CompanyServiceImpl companyService = new CompanyServiceImpl(companyRepository);
-        CreateCompanyDTO existingCnpjDto = new CreateCompanyDTO(validCnpj, 10.0, "existing.company@example.com", "123456789", "some-password");
-        when(companyRepository.findByCnpj(existingCnpjDto.cnpj())).thenReturn(Optional.of(new Company()));
+        CreateCompanyDTO existingCnpjDto = new CreateCompanyDTO(validCnpj, 10.0, validName, "existing.company@example.com", "123456789", "some-password");
+        when(companyRepository.findByCnpj(CNPJUtil.cleaning(existingCnpjDto.cnpj()))).thenReturn(Optional.of(new Company()));
         assertThrows(CompanyServiceException.class, () -> companyService.create(existingCnpjDto));
-        verify(companyRepository, times(1)).findByCnpj(existingCnpjDto.cnpj());
+        verify(companyRepository, times(1)).findByCnpj(CNPJUtil.cleaning(existingCnpjDto.cnpj()));
         verify(companyRepository, never()).save(any(Company.class));
     }
 }
